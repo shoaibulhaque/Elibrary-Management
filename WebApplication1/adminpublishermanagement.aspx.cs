c824df61d50665207ced76310d4cb629bc049c58
Shoaib <chaoderanony@gmail.com>
2023-04-18 18:34:40 +0500

    Publisher management

--- WebApplication1/adminpublishermanagement.aspx.cs
+++ WebApplication1/adminpublishermanagement.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,12 +10,38 @@ namespace WebApplication1
 {
     public partial class adminpublishermanagement : System.Web.UI.Page
     {
+        string strcon = ConfigurationManager.ConnectionStrings["con"].ConnectionString;  // Database  connection string
         protected void Page_Load(object sender, EventArgs e)
+        {
+
+
+
+
+        }
+
+
+        // Add button
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+
+        // Update  button
+        protected void Button1_Click(object sender, EventArgs e)
         {
 
         }
 
-        protected void Button2_Click(object sender, EventArgs e)
+        // Delete button
+        protected void Button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        // Go button
+        protected void Button21_Click(object sender, EventArgs e)
         {
 
         }
